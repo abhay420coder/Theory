51b35f0d69b9ca724c976dbc85bacbd244c868e4
Abhay Kumar <abhay.kumar@cordis.us>
2023-03-20 08:46:09 +0530

    - chat websocket app

---
+++ frontend developer/Angular/ANGULAR BY ARC/project/ws-chat-application/src/app/chat/chat.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+  // send:any;
+  sendMessage(sendForm:any){
+
+  }
+}
